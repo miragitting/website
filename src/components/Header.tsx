95216d489fbc6864c34c186bc781e649cc666d50
miragitting <m_gilespufahl@coloradocollege.edu>
2025-07-24 11:03:25 -0700

    skeleton of a home page, started Header component, moved globals.css to styles folder

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react';
+import '@/styles/globals.css';
+
+export const Header = () => {
+    return (
+        <header className="shadow-md flex py-8">
+        </header>
+    );
+};
